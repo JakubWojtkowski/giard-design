08c4a5af359e703bdccf508441e99ad1675fd462
wojtko <j.w2001@onet.pl>
2023-08-16 19:13:36 +0200

    Create navbar and intro section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,32 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
-
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      white: "#ffffff",
+      grey: "#F5F0EC",
+      beige: "#DCC1AB",
+      green: "#1B5B31",
+      black: "#111111",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+};
