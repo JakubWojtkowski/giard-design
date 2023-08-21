ecfa3e1ee15c384561de3f0c4586250ed2855e42
wojtko <j.w2001@onet.pl>
2023-08-21 15:52:10 +0200

    ADD: Gallery expand or hide functionality. Improve styles

--- src/main.js
+++ src/main.js
@@ -1,5 +1,50 @@
-// AddEventListener onClick
+const btn = document.getElementById("expand-btn");
+const gallery = document.querySelector(".gallery-container");
+const galleryCover = document.querySelector(".gallery-cover");
+const btnText = document.getElementById("expand-btn-text");
 
-// dostac sie do gallery-container -> zmienic max-height
-// dostac sie do gallery-cover -> usunac klase z tego diva
-// "Rozwiń" button: zmienic display na display: none
+let isExpanded = false;
+
+// functions
+
+const expandGallery = () => {
+  isExpanded = !isExpanded;
+  btnText.textContent = "Zwiń";
+
+  gallery.style.maxHeight = `${4000}px`;
+  galleryCover.style.display = "none";
+  gallery.style.marginBottom = `${100}px`;
+  btn.style.bottom = `${40}px`;
+  const arrow = (document.getElementById("expand-btn-arrow").style.transform =
+    "rotate(180deg)");
+};
+
+const hideGallery = () => {
+  isExpanded = !isExpanded;
+  document.getElementById("expand-btn-text").textContent = "Rozwiń";
+  gallery.style.maxHeight = `${600}px`;
+  galleryCover.style.display = "block";
+
+  gallery.style.marginBottom = `${0}px`;
+  btn.style.bottom = `${80}px`;
+  const arrow = (document.getElementById("expand-btn-arrow").style.transform =
+    "rotate(180deg)");
+};
+
+// listeners
+
+btn.addEventListener("click", () => {
+  isExpanded ? hideGallery() : expandGallery();
+});
+
+btn.addEventListener("mousemove", () => {
+  document
+    .getElementById("expand-btn-arrow-path")
+    .setAttribute("fill", "#f9f9f9");
+});
+
+btn.addEventListener("mouseleave", () => {
+  document
+    .getElementById("expand-btn-arrow-path")
+    .setAttribute("fill", "#111111");
+});
